dd9360266763b7bda1b7e0cfb59e1f2d87c794ec
Gift Nnko <nnkogift@hotmail.com>
2023-11-30 15:14:59 +0300

    fix(panel): Fixes
    - Fixed issues with build failing

--- apps/panel/next.config.js
+++ apps/panel/next.config.js
@@ -1,10 +1,17 @@
 /** @type {import("next").NextConfig} */
 const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 const nextConfig = {
 	output: "standalone",
 	basePath: process.env.NEXT_PUBLIC_CONTEXT_PATH,
 	experimental: {
 		outputFileTracingRoot: path.join(__dirname, "../../"),
+	},
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			config.externals = nodeExternals();
+		}
+		return config;
 	},
 };
 
