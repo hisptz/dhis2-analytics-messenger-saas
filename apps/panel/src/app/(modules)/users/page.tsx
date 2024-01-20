75c8013c0f4ea3689671cd30caabd1a47bb31947
Gift Nnko <nnkogift@hotmail.com>
2024-01-20 18:25:31 +0300

    Refactor user table rendering in users page
    The user table rows in the users page have been refactored into a separate component for improved code readability and maintainability. This change separates concerns properly by moving each table row's details and actions into a unique component named 'Row'. All table row logic and rendering are now handled within this new component.

--- apps/panel/src/app/(modules)/users/page.tsx
+++ apps/panel/src/app/(modules)/users/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ParseClient } from "@/utils/parse/client";
-import { useQuery } from "@tanstack/react-query";
+import { QueryObserverBaseResult, useQuery } from "@tanstack/react-query";
 import { FullLoader } from "@/components/FullLoader";
 import {
 	IconButton,
@@ -13,19 +13,21 @@ import {
 	TableHead,
 	TableRow,
 } from "@mui/material";
-import { useMemo, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import { MoreHoriz } from "@mui/icons-material";
 
-export default function Users() {
+function Row({
+	row,
+	refetch,
+}: {
+	row: Record<string, any>;
+	refetch: QueryObserverBaseResult<any, any>["refetch"];
+}) {
 	const currentUser = ParseClient.User.current();
+	const ref = useRef<HTMLButtonElement>(null);
 	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-	const open = Boolean(anchorEl);
 
-	async function getUsers() {
-		const query = new ParseClient.Query(ParseClient.User);
-		query.include(["approvedBy"]);
-		return query.find();
-	}
+	const open = Boolean(anchorEl);
 
 	const handleClose = () => {
 		setAnchorEl(null);
@@ -33,11 +35,6 @@ export default function Users() {
 	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
 		setAnchorEl(event.currentTarget);
 	};
-
-	const { data, isLoading, refetch } = useQuery({
-		queryKey: ["users"],
-		queryFn: getUsers,
-	});
 
 	function approveUser(user: ParseClient.User) {
 		return async () => {
@@ -48,6 +45,59 @@ export default function Users() {
 			await refetch();
 		};
 	}
+
+	return (
+		<TableRow key={`${row.object.id}-row`}>
+			<TableCell>
+				{row.fullName} {currentUser!.id === row.object.id ? "(me)" : ""}
+			</TableCell>
+			<TableCell>{row.username}</TableCell>
+			<TableCell>{row.emailVerified ? "Yes" : "No"}</TableCell>
+			<TableCell>{row.approved ? "Yes" : "No"}</TableCell>
+			<TableCell>
+				{row.approvedBy?.get("fullName") ??
+					row.approvedBy?.get("username")}
+			</TableCell>
+			<TableCell>
+				<IconButton
+					ref={ref}
+					key={`${row.objectId}-action-button`}
+					onClick={handleClick}
+					aria-controls={open ? "basic-menu" : undefined}
+					aria-haspopup="true"
+					aria-expanded={open ? "true" : undefined}
+				>
+					<MoreHoriz />
+				</IconButton>
+				<Menu
+					id="basic-menu"
+					anchorEl={anchorEl}
+					open={open}
+					onClose={handleClose}
+					MenuListProps={{
+						"aria-labelledby": "basic-button",
+					}}
+				>
+					<MenuItem onClick={approveUser(row.object)}>
+						{row.approved ? "Revoke approval" : "Approve"}
+					</MenuItem>
+				</Menu>
+			</TableCell>
+		</TableRow>
+	);
+}
+
+export default function Users() {
+	async function getUsers() {
+		const query = new ParseClient.Query(ParseClient.User);
+		query.include(["approvedBy"]);
+		return query.find();
+	}
+
+	const { data, isLoading, refetch } = useQuery({
+		queryKey: ["users"],
+		queryFn: getUsers,
+	});
 
 	const rows = useMemo(() => {
 		return data?.map(
@@ -76,62 +126,11 @@ export default function Users() {
 						<TableBody>
 							{rows?.map((row) => {
 								return (
-									<TableRow key={`${row.object.id}`}>
-										<TableCell>
-											{row.fullName}{" "}
-											{currentUser!.id === row.object.id
-												? "(me)"
-												: ""}
-										</TableCell>
-										<TableCell>{row.username}</TableCell>
-										<TableCell>
-											{row.emailVerified ? "Yes" : "No"}
-										</TableCell>
-										<TableCell>
-											{row.approved ? "Yes" : "No"}
-										</TableCell>
-										<TableCell>
-											{row.approvedBy?.get("fullName") ??
-												row.approvedBy?.get("username")}
-										</TableCell>
-										<TableCell>
-											<IconButton
-												key={`${row.objectId}-action-button`}
-												onClick={handleClick}
-												aria-controls={
-													open
-														? "basic-menu"
-														: undefined
-												}
-												aria-haspopup="true"
-												aria-expanded={
-													open ? "true" : undefined
-												}
-											>
-												<MoreHoriz />
-											</IconButton>
-											<Menu
-												id="basic-menu"
-												anchorEl={anchorEl}
-												open={open}
-												onClose={handleClose}
-												MenuListProps={{
-													"aria-labelledby":
-														"basic-button",
-												}}
-											>
-												<MenuItem
-													onClick={approveUser(
-														row.object,
-													)}
-												>
-													{row.approved
-														? "Revoke approval"
-														: "Approve"}
-												</MenuItem>
-											</Menu>
-										</TableCell>
-									</TableRow>
+									<Row
+										key={`${row.object.id}`}
+										row={row}
+										refetch={refetch}
+									/>
 								);
 							})}
 						</TableBody>
